6f499e8539f3199cd720ccb653ea7135c37783cf
mytkom <marek.mytkowski.mm@gmail.com>
2024-10-31 23:45:23 +0100

    consistent htmx request check

--- internal/middleware/htmx.go
+++ internal/middleware/htmx.go
@@ -2,12 +2,14 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/mytkom/AliceTraINT/internal/utils"
 )
 
 func NewValidateHTMXMw() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.Header.Get("HX-Request") != "true" {
+			if !utils.IsHTMXRequest(r) {
 				http.Error(w, "invalid request, accepts only HTMX", http.StatusUnprocessableEntity)
 			} else {
 				next.ServeHTTP(w, r)
@@ -19,7 +21,7 @@ func NewValidateHTMXMw() Middleware {
 func NewBlockHTMXMw() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.Header.Get("HX-Request") == "true" {
+			if utils.IsHTMXRequest(r) {
 				http.Error(w, "invalid request, does not accept HTMX", http.StatusUnprocessableEntity)
 			} else {
 				next.ServeHTTP(w, r)
