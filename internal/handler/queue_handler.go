cc9c035fe95fe3897ad27cf17e7c41ae0c50534a
mytkom <marek.mytkowski.mm@gmail.com>
2024-11-16 17:24:57 +0100

    add authorization of training machine

--- internal/handler/queue_handler.go
+++ internal/handler/queue_handler.go
@@ -1,3 +1,34 @@
 package handler
 
-// TODO
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/mytkom/AliceTraINT/internal/db/models"
+	"github.com/mytkom/AliceTraINT/internal/db/repository"
+	"github.com/mytkom/AliceTraINT/internal/hash"
+)
+
+type QueryHandler struct {
+	TrainingMachineRepo repository.TrainingMachineRepository
+}
+
+func (qh *QueryHandler) getAuthorizedTrainingMachine(r *http.Request, tmId uint) (*models.TrainingMachine, error) {
+	secretId := r.Header.Get("Secret-Id")
+
+	trainingMachine, err := qh.TrainingMachineRepo.GetByID(tmId)
+	if err != nil {
+		return nil, err
+	}
+
+	ok, err := hash.VerifyKey(secretId, trainingMachine.SecretKeyHashed)
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		return nil, fmt.Errorf("authorization failure")
+	}
+
+	return trainingMachine, nil
+}
