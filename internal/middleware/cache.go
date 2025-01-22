86e2bafe4abab466a1798708d5ea749fca9c3bf4
mytkom <marek.mytkowski.mm@gmail.com>
2025-01-22 11:37:15 +0100

    Cache only success responses and responsive UI adjustments

--- internal/middleware/cache.go
+++ internal/middleware/cache.go
@@ -11,7 +11,13 @@ import (
 
 type myResponseWriter struct {
 	http.ResponseWriter
-	buf *bytes.Buffer
+	buf    *bytes.Buffer
+	status int
+}
+
+func (mrw *myResponseWriter) WriteHeader(code int) {
+	mrw.status = code
+	mrw.ResponseWriter.WriteHeader(code)
 }
 
 func (mrw *myResponseWriter) Write(p []byte) (int, error) {
@@ -21,6 +27,7 @@ func (mrw *myResponseWriter) Write(p []byte) (int, error) {
 func NewCacheMw(c *utils.Cache) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Cache only GET requests
 			if r.Method != "GET" {
 				next.ServeHTTP(w, r)
 				return
@@ -35,23 +42,30 @@ func NewCacheMw(c *utils.Cache) Middleware {
 				return
 			}
 
-			// if not cached pass it to the next handlerFunc
+			// if not cached, pass it to the next handlerFunc
 			mrw := &myResponseWriter{
 				ResponseWriter: w,
 				buf:            &bytes.Buffer{},
+				status:         http.StatusOK, // Default to 200
 			}
 
 			next.ServeHTTP(mrw, r)
 
-			cacheBuffer := bytes.Buffer{}
-			multiWriter := io.MultiWriter(w, &cacheBuffer)
+			// Cache only 200 OK responses
+			if mrw.status == http.StatusOK {
+				cacheBuffer := bytes.Buffer{}
+				multiWriter := io.MultiWriter(w, &cacheBuffer)
 
-			if _, err := io.Copy(multiWriter, mrw.buf); err != nil {
-				log.Printf("Failed to send out response from CacheMiddleware")
+				if _, err := io.Copy(multiWriter, mrw.buf); err != nil {
+					log.Printf("Failed to send out response from CacheMiddleware")
+				}
+
+				// Save to cache
+				c.Set(cacheKey, cacheBuffer.String())
+			} else {
+				// Write response directly if not 200 OK
+				_, _ = io.Copy(w, mrw.buf)
 			}
-
-			// cache result
-			c.Set(cacheKey, cacheBuffer.String())
 		})
 	}
 }
