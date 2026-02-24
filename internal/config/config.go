26d34892593a9a9ce87fbc29411c9fb6a9f6aad6
mytkom <marek.mytkowski.mm@gmail.com>
2026-02-24 12:03:09 +0100

    make .env file non mandatory for prod deployment

--- internal/config/config.go
+++ internal/config/config.go
@@ -34,7 +34,7 @@ type DatabaseConfig struct {
 func LoadConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	return &Config{
